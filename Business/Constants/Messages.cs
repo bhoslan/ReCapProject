4030a75d9e9642ba3fa0b63790c09ff6e91248b9
bhoslan <bhoslan@gmail.com>
2021-12-13 11:16:21 +0300

    12.gün FluentValidation

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,27 +6,36 @@ namespace Business.Constants
 {
     public class Messages
     {
-        public static string BrandAdded="Brand added";
-        public static string BrandDeleted="Brand deleted";
-        public static string BrandUpdated="Brand updated";
-        public static string CarAdded="Car added";
-        public static string CarDeleted="Car deleted";
-        public static string CarUpdated="Car updated";
-        public static string CategoryAdded="Category added";
-        public static string CategoryDeleted="Category deleted";
-        public static string CategoryUpdated="Category updated";
-        public static string ColorAdded="Color added";
-        public static string ColorDeleted="Color deleted";
-        public static string ColorUpdated="Color updated";
-        public static string UserAdded="User added";
-        public static string UserDeleted="User deleted";
-        public static string UserUpdated="User updated";
-        public static string CustomerAdded="Customer added";
-        public static string CustomerDeleted="Customer deleted";
-        public static string CustomerUpdated="Customer updated";
-        public static string RentalAdded="Rental car added";
-        public static string RentalDeleted="Rental car deleted";
-        public static string RentalUpdated="Rental car updated";
-        public static string CarAlreadyRented="Car is already rented";
+        public static string BrandAdded="Marka eklendi";
+        public static string BrandDeleted="Marka silindi";
+        public static string BrandUpdated="Marka güncellendi";
+
+        public static string CarAdded="Araç eklendi";
+        public static string CarDeleted="Araç silindi";
+        public static string CarUpdated="Araç güncellendi";
+
+        public static string CategoryAdded="Kategori eklendi";
+        public static string CategoryDeleted="Kategori silindi";
+        public static string CategoryUpdated="Kategori güncellendi";
+
+        public static string ColorAdded="Renk eklendi";
+        public static string ColorDeleted="Renk silindi";
+        public static string ColorUpdated="Renk güncellendi";
+
+        public static string UserAdded="Kullanıcı bilgisi eklendi";
+        public static string UserDeleted="Kullanıcı bilgisi silindi";
+        public static string UserUpdated="Kullanıcı bilgisi güncellendi";
+
+        public static string CustomerAdded="Müşteri bilgisi eklendi";
+        public static string CustomerDeleted="Müşteri bilgisi silindi";
+        public static string CustomerUpdated="Müşteri bilgisi güncellendi";
+        
+        public static string RentalAdded="Araç kiralandı";
+        public static string RentalDeleted="Kiralık araç silindi";
+        public static string RentalUpdated="Kiralık araç bilgisi güncellendi";
+       
+        public static string CarAlreadyRented="Araç şu an kirada";
+        
+        public static string CheckDate="Geçersiz tarih girdiniz";
     }
 }
---
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,22 @@
+﻿using Business.Constants;
+using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator : AbstractValidator<Rental>
+    {
+        public RentalValidator()
+        {
+            RuleFor(r => r).Must(CheckDate).WithMessage(Messages.CheckDate);
+        }
+
+        private bool CheckDate(Rental arg)
+        {
+            return arg.RentDate > arg.ReturnDate;
+        }
+    }
+}
